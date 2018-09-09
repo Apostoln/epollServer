63bab0f8ea83ee0b92bfc985bbce31ebabba38cf
Nik Bond <nikbond97@gmail.com>
2018-09-09 17:49:36 +0300

    Add draft of poll wrapper

--- Server.h
+++ Server.h
@@ -2,12 +2,15 @@
 #define EPOLLSERVER_SERVER_H
 
 #include <vector>
+#include <set>
 #include "Socket.h"
 
 class Server {
 private:
     Socket mMasterSocket;
     size_t mPort;
+
+    //Poll mPoll;
 
     std::vector<Socket> mSlaveSockets;
 
@@ -21,12 +24,39 @@ public:
 
         std::cout << "Server run on localhost:" << mPort << std::endl;
 
+/*
+ * Draft of poll:
+        mPoll.add(Socket, Poll::POLLIN);
+        while(true) {
+            int timeout = 10;
+            std::set<Socket> result = mPoll.select(timeout);
+            for(auto socket : result) {
+                if (socket == mMasterSocket) { //todo: add event to condition
+                    Socket newClientSocket = mMasterSocket.accept();
+                    mPoll.add(newClientSocket, Poll::POLLIN);
+                    mSlaveSockets.push_back(Socket);
+                }
+                else { //todo: add event to condition
+                    auto message = socket.receive();
+                    std::cout << message << std::endl;
+                    socket.send(message);
+                }
+
+            }
+
+        }
+*/
+
+
+
         while(Socket clientSocket = mMasterSocket.accept()) {
             auto message = clientSocket.receive();
             std::cout << message << std::endl;
             clientSocket.send(message);
             //mSlaveSockets.push_back(std::move(clientSocket));
         }
+
+
     }
 };
 #endif //EPOLLSERVER_SERVER_H
